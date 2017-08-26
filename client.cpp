5e3be7eee01de742048ef185bc40f45a0d17b41e
hareesh <hareesh.dx@gmail.com>
2017-08-26 23:27:57 +0530

    Hello world Done

--- client.cpp
+++ client.cpp
@@ -7,28 +7,24 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-void error(const char *msg)
-{
+#define HOST "10.129.23.200"
+#define PORT 9512
+
+int sockfd, portno, n;
+struct sockaddr_in serv_addr;
+struct hostent *server;
+char buffer[256];
+
+void error(const char *msg){
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"Usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
+void establishConenction(){
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    error("ERROR opening socket");
+    server = gethostbyname(HOST);
     if (server == NULL) {
         fprintf(stderr,"ERROR,Host not found\n");
         exit(0);
@@ -36,24 +32,37 @@ int main(int argc, char *argv[])
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
+    (char *)&serv_addr.sin_addr.s_addr,
+    server->h_length);
+    serv_addr.sin_port = htons(PORT);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
+    error("ERROR connecting");
+}
 
+int sendMessage(char *msg){
+        n = write(sockfd,msg,strlen(buffer));
+        if (n < 0)
+        error("ERROR writing to socket");
+        bzero(buffer,256);
+        n = read(sockfd,buffer,255);
+        if (n < 0)
+        error("ERROR reading from socket");
+        return atoi(buffer);
+}
 
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0)
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+void closeConnection(){
     close(sockfd);
+}
+
+int main(int argc, char *argv[]){
+    establishConenction();
+    while(true){
+        printf("Please enter the message: ");
+        bzero(buffer,256);
+        fgets(buffer,255,stdin);
+        int response=sendMessage(buffer);
+        printf("Response code %d \n",response);
+    }
+    closeConnection();
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,5 +1,4 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+#include<iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,50 +7,75 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(const char *msg)
-{
+#define PORT 9512
+
+//Global paramaters
+int sockfd, newsockfd, portno;
+socklen_t clilen;
+char buffer[256];
+struct sockaddr_in serv_addr, cli_addr;
+
+using namespace std;
+
+void error(const char *msg){
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd,
-                 (struct sockaddr *) &cli_addr,
-                 &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     while(true){
-       bzero(buffer,256);
-       n = read(newsockfd,buffer,255);
-       if (n < 0) error("ERROR reading from socket");
-       printf("Here is the message: %s\n",buffer);
-       n = write(newsockfd,"I got your message",18);
-       if (n < 0) error("ERROR writing to socket");
-     }
-     close(newsockfd);
-     close(sockfd);
+//socket, bind, listen
+void establishConenction(){
+    printf("Starting server ....\n");
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+    sizeof(serv_addr)) < 0)
+    error("ERROR on binding");
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    printf("Server started... Waiting for connection...\n");
+    newsockfd = accept(sockfd,
+        (struct sockaddr *) &cli_addr,
+        &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
+}
+
+//process message and sent response
+int onMessage(char *buffers){
+    printf("Here is the message: %s\n",buffer);
+    return 0;
+}
+
+//need to add case statement and add a way to close the connenction
+void onRecieve(){
+        int n;
+        while(true){
+            bzero(buffer,256);
+            n = read(newsockfd,buffer,255);
+            if (n < 0) error("ERROR reading from socket");
+            char response[3];
+            sprintf(response,"%d",onMessage(buffer));
+            n = write(newsockfd,response,strlen(response));
+            if (n < 0) error("ERROR writing to socket");
+        }
+}
+
+
+
+void closeConnection(){
+    close(newsockfd);
+    close(sockfd);
+}
+
+
+int main(int argc, char *argv[]){
+     establishConenction();
+     onRecieve();
+     closeConnection();
      return 0;
 }
